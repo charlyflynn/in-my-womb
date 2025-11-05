629982b96c2cb542bf27fee2a42459d636fd2c12
Charlie Flynn <35696585+charlyflynn@users.noreply.github.com>
2025-11-05 16:24:32 +0000

    add download button to end

--- src/game/scenes/Fin.js
+++ src/game/scenes/Fin.js
@@ -15,16 +15,28 @@ export default class Fin extends Scene {
         };
 
         this.add
-            .text(dims.w / 2, dims.h / 2, "YOU\nWON!", {
+            .text(dims.w / 2, dims.h / 2, "CLAIM\nYOUR\nPRIZE", {
                 fontFamily: "Arial Black",
-                fontSize: 100,
-                color: "#cccccc",
+                fontSize: 72,
+                color: "#333333",
+                padding: 30,
+                backgroundColor: "#cccccc",
                 align: "center",
                 letterSpacing: 20,
                 wordWrap: { width: 450, useAdvancedWrap: false },
             })
             .setOrigin(0.5)
-            .setDepth(100);
+            .setDepth(100)
+            .setInteractive({ useHandCursor: true })
+            .on("pointerup", () => {
+                const url = "assets/download/WMI.mp3";
+                const a = document.createElement("a");
+                a.href = url;
+                a.download = url.split("/").pop();
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+            });
 
         EventBus.emit("current-scene-ready", this);
         this.cameras.main.fadeIn(1500);
