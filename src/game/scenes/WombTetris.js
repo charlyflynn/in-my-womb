786dcc2578192de2f7ba7ec95b2e56f6ea34cd77
Charlie Flynn <35696585+charlyflynn@users.noreply.github.com>
2025-11-19 14:42:48 +0000

    unbreak my heart

--- src/game/scenes/WombTetris.js
+++ src/game/scenes/WombTetris.js
@@ -68,7 +68,7 @@ export default class WombTetris extends Phaser.Scene {
                 },
             },
             {
-                // key: "wombVox",
+                key: "wombVox",
                 shape: "womb-gemas",
                 position: {
                     x: 1080 - 230,
