ea8d313e6e4ba373ad31b2c438e6231ea4b03ccc
Charlie Flynn <35696585+charlyflynn@users.noreply.github.com>
2025-11-19 14:41:07 +0000

    less obnoxious ending fades

--- src/game/scenes/Fin.js
+++ src/game/scenes/Fin.js
@@ -10,7 +10,7 @@ export default class Fin extends Scene {
         const bg = this.add.image(0, 0, "bgFin").setOrigin(0, 0);
         const endSequence = () => {
             this.cameras.main
-                .fadeOut(1500, 255, 255, 255)
+                .fadeOut(1000, 255, 255, 255)
                 .on("camerafadeoutcomplete", () => {
                     bg.destroy();
                     gratitude.destroy();
@@ -23,7 +23,7 @@ export default class Fin extends Scene {
                     success.style.display = "block";
                     // this.cameras.main.setBackgroundColor(0xffffff);
                     this.cameras.main
-                        .fadeIn(1500, 255, 255, 255)
+                        .fadeIn(1000, 255, 255, 255)
                         .on("camerafadeincomplete", () => {
                             success.style.zIndex = 0;
                         });
